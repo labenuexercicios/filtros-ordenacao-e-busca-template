6a4018f01ee3e726fb2fb4b8390b425c4e546bea
rodnikerson <nikersonrodolpho@gmail.com>
2022-11-30 21:26:16 -0300

    aula 30.11

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import pokemons from "./pokemon/pokemon.json";
 import PokemonCard from "./components/PokemonCard/PokemonCard";
 import { getColors } from "./utils/ReturnCardColor";
 import Header from "./components/Header/Header.js";
+import { useState } from "react";
 const GlobalStyle = createGlobalStyle`
-  *{
+  * {
     padding: 0;
     margin: 0;
     box-sizing: border-box;
     font-family: "Inter", sans-serif;
-  
+  }
+  body {
+    background: #404040;
   }
 `;
 const CardsContainer = styled.div`
@@ -18,18 +21,41 @@ const CardsContainer = styled.div`
   justify-items: center;
 `;
 function App() {
+  const [id, setId] = useState('')
+  const [name, setName] = useState('')
+
+  const allPokes = pokemons.map((pokemon) => {
+    return <PokemonCard
+      cardColor={getColors(pokemon.type[0])}
+      key={pokemon.id}
+      pokemon={pokemon}
+    />
+  })
+
+  const pokesFilteredById = pokemons.filter(pokemon => pokemon.id.includes(id))
+    .map((pokemon) => {
+      return <PokemonCard
+        cardColor={getColors(pokemon.type[0])}
+        key={pokemon.id}
+        pokemon={pokemon}
+      />
+    })
+
+  const pokesFilteredByName = pokemons.filter(pokemon => pokemon.name.english.toLowerCase().includes(name.toLowerCase()))
+    .map((pokemon) => (
+      <PokemonCard
+        cardColor={getColors(pokemon.type[0])}
+        key={pokemon.id}
+        pokemon={pokemon}
+      />
+    ))
+
   return (
     <>
       <GlobalStyle />
-      <Header />
+      <Header id={id} setId={setId} name={name} setName={setName} />
       <CardsContainer>
-        {pokemons.map((pokemon) => {
-          return <PokemonCard
-          cardColor={getColors(pokemon.type[0])}
-          key={pokemon.id}
-          pokemon={pokemon}
-        />
-        })}
+        {id ? pokesFilteredById : name ? pokesFilteredByName : allPokes}
       </CardsContainer>
     </>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container } from "./styles";
 
-const Header = () => {
+const Header = ({id, setId, name, setName}) => {
   const pokemontypesArray = [
     "Normal",
     "Fire",
@@ -25,8 +25,8 @@ const Header = () => {
 
   return (
     <Container>
-      <input type="number" placeholder="Buscar por id" />
-      <input type="text" placeholder="Buscar por nome" />
+      <input type="number" placeholder="Buscar por id" value={id} onChange={(e) => setId(e.target.value)} />
+      <input type="text" placeholder="Buscar por nome" value={name} onChange={(e) => setName(e.target.value)} />
       <select>
         <option value="">Ordenar</option>
         <option value="">Crescente</option>
